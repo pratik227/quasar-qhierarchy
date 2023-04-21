7ac8547fd9a4dc30ce6db61359770a4783415680
pratikpatel <pratikpatelpp802@gmail.com>
2023-04-21 12:06:37 +0530

    Header Slot and Row click

---
+++ demo/src/pages/HeaderSlotDemoExample.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-page>
+    <div :class="($q.platform.is.mobile ? '' : 'q-mx-xl')">
+
+      <div class="markdown-nav-bar shadow-10">
+        <div class="title">
+          <span class="ellipsis">{{ title }}</span>
+        </div>
+      </div>
+
+      <example-viewer
+        title="QHierarchy Slot Demo"
+        file="SlotDemo"
+      />
+
+      <page-footer></page-footer>
+
+    </div>
+  </q-page>
+</template>
+
+<script>
+import {defineAsyncComponent, defineComponent, ref} from 'vue'
+
+const year = (new Date()).getFullYear()
+
+export default defineComponent({
+  name: "HeaderSlotDemo",
+  setup(){
+    return{
+      title:'QHierarchy Header Slot Demo',
+      year,
+      openLink(href) {
+        window.open(href)
+      }
+    }
+  },
+  components:{
+    PageFooter: defineAsyncComponent(() => import('components/PageFooter')),
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
